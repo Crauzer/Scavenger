2fb2f6c9b84d1a16dada97805cf4df35aac54ceb
Crauzer <filip.quitko@gmail.com>
2021-05-01 18:28:03 +0200

    map ui imporvements

--- Scavenger/MVVM/ViewModels/BinTreeMapViewModel.cs
+++ Scavenger/MVVM/ViewModels/BinTreeMapViewModel.cs
@@ -10,7 +10,7 @@ namespace Scavenger.MVVM.ViewModels
 {
     public class BinTreeMapViewModel : BinTreeParentViewModel
     {
-        public override string Header => $"{this.Name} -> {this.TreeProperty.Type}<{(this.TreeProperty as BinTreeMap).KeyType}, {(this.TreeProperty as BinTreeMap).ValueType}>";
+        public string Metadata => $" -> {this.TreeProperty.Type}<{(this.TreeProperty as BinTreeMap).KeyType}, {(this.TreeProperty as BinTreeMap).ValueType}>";
 
         public BinTreeMapViewModel(BinTreeParentViewModel parent, BinTreeMap treeProperty) : base(parent, treeProperty)
         {

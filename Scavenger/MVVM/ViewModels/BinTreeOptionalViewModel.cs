6e6633a0f08dd6a69f38fadbe9fd7641baa79417
Crauzer <filip.quitko@gmail.com>
2021-05-02 20:40:23 +0200

    update optional UI

--- Scavenger/MVVM/ViewModels/BinTreeOptionalViewModel.cs
+++ Scavenger/MVVM/ViewModels/BinTreeOptionalViewModel.cs
@@ -11,11 +11,24 @@ namespace Scavenger.MVVM.ViewModels
     {
         public string Metadata => $"Optional<{(this.TreeProperty as BinTreeOptional).ValueType}>";
 
+        public bool IsSome
+        {
+            get => this._isSome;
+            set
+            {
+                this._isSome = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool _isSome;
+
         public BinTreeOptionalViewModel(BinTreeParentViewModel parent, BinTreeOptional treeProperty) : base(parent, treeProperty)
         {
             BinTreePropertyViewModel valueViewModel = BinTreeUtilities.ConstructTreePropertyViewModel(this, treeProperty.Value);
             if (valueViewModel is not null)
             {
+                this.IsSome = true;
                 valueViewModel.ShowName = false;
 
                 this.Children.Add(valueViewModel);
@@ -26,12 +39,18 @@ namespace Scavenger.MVVM.ViewModels
         {
             BinTreeOptional treeOptional = this.TreeProperty as BinTreeOptional;
 
-            return this.Children.Count switch
+            if (this.Children.Count == 0)
             {
-                0 => new BinTreeOptional(null, this.NameHash, treeOptional.ValueType, null),
-                1 => new BinTreeOptional(null, this.NameHash, treeOptional.ValueType, this.Children[0].BuildProperty()),
-                _ => throw new InvalidOperationException("Optional Property must contain one child")
-            };
+                return new BinTreeOptional(null, this.NameHash, treeOptional.ValueType, null);
+            }
+            else if(this.Children.Count == 1 && this.IsSome)
+            {
+                return new BinTreeOptional(null, this.NameHash, treeOptional.ValueType, this.Children[0].BuildProperty());
+            }
+            else
+            {
+                throw new InvalidOperationException("Optional Property must contain one child");
+            }
         }
     }
 }

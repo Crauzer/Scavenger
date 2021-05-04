4fdde8ab7a37dd76bc9251e65a0a408a443aa11c
Crauzer <filip.quitko@gmail.com>
2021-05-04 21:36:02 +0200

    fix Optional sync bug

--- Scavenger/MVVM/ViewModels/BinTreeProperties/BinTreeOptionalViewModel.cs
+++ Scavenger/MVVM/ViewModels/BinTreeProperties/BinTreeOptionalViewModel.cs
@@ -10,8 +10,17 @@ namespace Scavenger.MVVM.ViewModels
 {
     public class BinTreeOptionalViewModel : BinTreeParentViewModel
     {
-        [JsonIgnore] public string Metadata => $"Optional<{(this.TreeProperty as BinTreeOptional).ValueType}>";
+        [JsonIgnore] public string Metadata => $"Optional<{this.ValueType}>";
 
+        public BinPropertyType ValueType
+        {
+            get => this._valueType;
+            set
+            {
+                this._valueType = value;
+                NotifyPropertyChanged();
+            }
+        }
         public bool IsSome
         {
             get => this._isSome;
@@ -19,14 +28,18 @@ namespace Scavenger.MVVM.ViewModels
             {
                 this._isSome = value;
                 NotifyPropertyChanged();
+                SyncTreeProperty();
             }
         }
 
+        private BinPropertyType _valueType;
         private bool _isSome;
 
         public BinTreeOptionalViewModel() : base(null, null, new BinTreeOptional(null, 0, BinPropertyType.None, null)) { }
         public BinTreeOptionalViewModel(BinTreeParentViewModel parent, BinTreeOptional treeProperty) : base(parent.BinTree, parent, treeProperty)
         {
+            this.ValueType = treeProperty.ValueType;
+
             BinTreePropertyViewModel valueViewModel = BinTreeUtilities.ConstructTreePropertyViewModel(this, treeProperty.Value);
             if (valueViewModel is not null)
             {
@@ -37,17 +50,20 @@ namespace Scavenger.MVVM.ViewModels
             }
         }
 
+        public override void SyncTreeProperty()
+        {
+            this.TreeProperty = BuildProperty();
+        }
+
         public override BinTreeProperty BuildProperty()
         {
-            BinTreeOptional treeOptional = this.TreeProperty as BinTreeOptional;
-
             if (this.Children.Count == 0)
             {
-                return new BinTreeOptional(null, this.NameHash, treeOptional.ValueType, null);
+                return new BinTreeOptional(null, this.NameHash, this.ValueType, null);
             }
             else if (this.Children.Count == 1 && this.IsSome)
             {
-                return new BinTreeOptional(null, this.NameHash, treeOptional.ValueType, this.Children[0].BuildProperty());
+                return new BinTreeOptional(null, this.NameHash, this.ValueType, this.Children[0].BuildProperty());
             }
             else
             {

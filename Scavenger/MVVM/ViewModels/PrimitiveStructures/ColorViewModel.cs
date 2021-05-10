8b9cdd03dbeb81ef5f6e44b0bc04d723cd9ba6a3
Crauzer <filip.quitko@gmail.com>
2021-05-10 12:37:45 +0200

    ColorViewModel

---
+++ Scavenger/MVVM/ViewModels/PrimitiveStructures/ColorViewModel.cs
@@ -0,0 +1,65 @@
+﻿using LeagueToolkit.Helpers.Structures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scavenger.MVVM.ViewModels.PrimitiveStructures
+{
+    public class ColorViewModel : PropertyNotifier
+    {
+        public float R
+        {
+            get => this._r;
+            set
+            {
+                this._r = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public float G
+        {
+            get => this._g;
+            set
+            {
+                this._g = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public float B
+        {
+            get => this._b;
+            set
+            {
+                this._b = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public float A
+        {
+            get => this._a;
+            set
+            {
+                this._a = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private float _r;
+        private float _g;
+        private float _b;
+        private float _a;
+
+        public ColorViewModel(Color color)
+        {
+            this.R = color.R;
+            this.G = color.G;
+            this.B = color.B;
+            this.A = color.A;
+        }
+        
+        public Color ToColor()
+        {
+            return new Color(this.R, this.G, this.B, this.A);
+        }
+    }
+}

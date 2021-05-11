1bf675f2dc626c2eaadee19bb588af12d6c2a6c0
Crauzer <filip.quitko@gmail.com>
2021-05-11 14:15:26 +0200

    editors for colors

---
+++ Scavenger/MVVM/ModelViews/PrimitiveStructures/ColorEditor.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Scavenger.MVVM.ViewModels.PrimitiveStructures;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scavenger.MVVM.ModelViews.PrimitiveStructures
+{
+    /// <summary>
+    /// Interaction logic for ColorEditor.xaml
+    /// </summary>
+    public partial class ColorEditor : UserControl, INotifyPropertyChanged
+    {
+        public ColorViewModel Color
+        {
+            get => (ColorViewModel)GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+
+        public static readonly DependencyProperty ColorProperty = DependencyProperty.Register(
+            "Color",
+            typeof(ColorViewModel),
+            typeof(ColorEditor),
+            new FrameworkPropertyMetadata(default(ColorViewModel), FrameworkPropertyMetadataOptions.AffectsRender, OnColorChanged));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ColorEditor()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnColorChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            ColorEditor control = (ColorEditor)dependencyObject;
+            if (eventArgs.NewValue is ColorViewModel color)
+            {
+                control.Color = color;
+            }
+        }
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Scavenger/MVVM/ModelViews/PrimitiveStructures/ValueColorEditor.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Scavenger.MVVM.ViewModels.PrimitiveStructures;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scavenger.MVVM.ModelViews.PrimitiveStructures
+{
+    /// <summary>
+    /// Interaction logic for ValueColorEditor.xaml
+    /// </summary>
+    public partial class ValueColorEditor : UserControl, INotifyPropertyChanged
+    {
+        public ValueColorViewModel Value
+        {
+            get => (ValueColorViewModel)GetValue(Color);
+            set => SetValue(Color, value);
+        }
+
+        public static readonly DependencyProperty Color = DependencyProperty.Register(
+            "Color",
+            typeof(ValueColorViewModel),
+            typeof(ValueColorEditor),
+            new FrameworkPropertyMetadata(default(ValueColorViewModel), FrameworkPropertyMetadataOptions.AffectsRender, OnValueChanged));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ValueColorEditor()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnValueChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            ValueColorEditor control = (ValueColorEditor)dependencyObject;
+            if (eventArgs.NewValue is ValueColorViewModel value)
+            {
+                control.Value = value;
+            }
+        }
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
